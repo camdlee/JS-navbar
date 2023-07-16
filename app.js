06c23ab68a06934c2157cba3c5c370f104a44282
Cameron Lee <camdlee18@gmail.com>
2023-07-16 14:05:54 -0400

    added eventListener for toggle and adding/removing class to navtoggle

--- app.js
+++ app.js
@@ -3,3 +3,26 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+
+// ------------ Assigning selections to variables -------------
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+
+//-------------- Events -----------------
+navToggle.addEventListener('click', ()=>{
+    //console.log(links.classList)
+    // console.log(links.classList.contains('links'))
+    
+    // adding or removing show links class to change the DOM
+    // if(links.classList.contains('show-links')){
+    //     links.classList.remove('show-links')
+    // } else {
+    //     links.classList.add('show-links')
+    // }
+
+    // one liner using the toggle method
+    links.classList.toggle('show-links')
+});
+
